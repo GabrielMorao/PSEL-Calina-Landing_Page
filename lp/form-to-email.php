59b517272522bea45f655ba2687ef7b6217013d8
GabrielMorao <38427124+GabrielMorao@users.noreply.github.com>
2020-03-21 20:49:24 -0300

    Add files via upload

--- lp/form-to-email.php
+++ lp/form-to-email.php
@@ -1,45 +1,19 @@
 <?php
 
-require 'C:\xampp\htdocs\lp\class.simple_mail.php';
+ini_set("SMTP","smtp.gmail.com");
+ini_set("smtp_port","587");
 
 if (isset($_POST['send'])) {
   $to = 'bielmorao@gmail.com';
+  $name = $_POST['name'];
+  $from = $_POST['email'];
   $subject = 'Contato do cliente';
-  $message = 'Name: '. $_POST['name']. "\r\n\r\n";
-  $message .= 'Email: '. $_POST['email']. "\r\n\r\n";
-  $message .= 'Mensagem: '. $_POST['mensagem']. "\r\n\r\n";
-
-  $send = SimpleMail::make()
-    ->setTo($to, 'gabriel')
-    ->setFrom($_POST['email'], $_POST['name'])
-    ->setSubject($subject)
-    ->setMessage($message)
-    ->setReplyTo($_POST['email'], $_POST['name'])
-    ->setHtml()
-    ->setWrap(100)
-    ->send();
+  $message = $_POST['mensagem'];
+  echo $subject, " from ", $name, " at ", $from, "<br/>";
+echo $message;
     
-echo ($send) ? 'Email sent successfully' : 'Could not send email';
+mail($to, $subject, $message);
 
 }
 
-$success = mail($to, $subject, $message);
-
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<?php if (isset($sucess) && $sucess){ ?>
-<h1>Obrigado</h1>
-Sua mensagem foi enviada.
-<?php } else{ ?>
-<h1>Oops!</h1>
-Desculpe, houve um problema enviando sua mensagem.
-<?php } ?>
-
-</body>
-</html>
